f9d880cc9ae6ab62b7547c193fe7e3d3873ca6cd
scal-o <alex.scalese@virgilio.it>
2026-01-23 19:12:12 +0100

    change: update sim_reps.py
    updates:
    - configurable confidence level
    - extract plotting function
    - use scipy.stats.t to dynamically compute the t-statistic

--- calibration/src/calibration/sim_reps.py
+++ calibration/src/calibration/sim_reps.py
@@ -4,10 +4,13 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import seaborn.objects as so
+from scipy.stats import t as tdist
 
+# set input and output directories
 INPUT_DIR = Path("data")
-OUTPUT_DIR = Path("results")
+OUTPUT_DIR = Path("results") / "plots"
 
+# set plotting options
 sns.set_theme(style="whitegrid")
 so.Plot.config.theme.update(sns.axes_style("whitegrid"))
 
@@ -22,80 +25,59 @@ stats["mean"] = data.mean(axis=1)
 stats["std"] = data.std(axis=1)
 stats["tol"] = 0.1 * stats["mean"]
 
-# t-students values from table (for n-1 = 1:15) @97.5%
-t_values = [
-    12.71,
-    4.303,
-    3.182,
-    2.776,
-    2.571,
-    2.447,
-    2.365,
-    2.306,
-    2.262,
-    2.228,
-    2.201,
-    2.179,
-    2.160,
-    2.145,
-    2.131,
-]
-t_n = [i for i in range(1, 16, 1)]
-prop = []
-pos_n = []
+# set test parameters
+confidence_level = 0.95
+alpha = 1 - confidence_level
+max_sim_n = 30
 
-for n, t in zip(t_n, t_values):
-    stats[f"{n}"] = ((2 * t * stats["std"]) / stats["tol"]) ** 2
+
+def plot_det_prop(num, det_n, title, output):
+    (
+        so.Plot()
+        .add(so.Bars(), x=np.linspace(1, max_sim_n, max_sim_n), y=100 * np.array(num) / det_n)
+        .limit(x=(0.2, max_sim_n + 0.2), y=(0, 100))
+        .label(
+            x="Number of simulations",
+            y="Detectors (%)",
+            title=title,
+        )
+        .save(OUTPUT_DIR / output, dpi=300)
+    )
+
+
+# create lists
+prop = []
+num = []
+
+for n in range(1, max_sim_n + 1):
+    # compute t-statistic
+    t_val = tdist.ppf(1 - alpha / 2, n)
+
+    # compute number of required sims
+    stats[f"{n}"] = ((t_val * stats["std"]) / stats["tol"]) ** 2
     stats = stats.fillna(0)
 
+    # check (for each link) if the number of required sims is under the t-student sample size
     stats[f"{n}"] = stats[f"{n}"] < (n + 1)
 
-    pos_n.append(stats[f"{n}"].sum())
+    # append number and proportion of significant detectors
+    num.append(stats[f"{n}"].sum())
     prop.append((stats[f"{n}"]).sum() / stats.shape[0])
 
-(
-    so.Plot()
-    .add(so.Bars(), x=np.linspace(1, 15, 15), y=pos_n)
-    .add(
-        so.Path(color="k", linewidth=2, linestyle="--"),
-        x=np.linspace(1, 15, 15),
-        y=len(stats),
-        label="Total detectors",
-    )
-    .limit(x=(0.2, 15.8))
-    .label(
-        x="Number of simulations",
-        y="Number of detectors",
-        title="Detectors with significant results (97.5%)",
-    )
-    .save(OUTPUT_DIR / "plots" / "stat_signif.png")
-)
+# create plots
+plot_det_prop(num, len(stats), r"Detectors within 97.5% confidence level", "stat_signif.png")
 
-s_mean = stats["mean"].mean()
-p = stats["mean"] > 0.05 * s_mean
-
+# filter links with average flow < 5% of the global average
+p = stats["mean"] > (0.05 * stats["mean"].mean())
 filt_stats = stats[p]
-filt_pos_n = [filt_stats[f"{n}"].sum() for n in t_n]
-
-(
-    so.Plot()
-    .add(so.Bars(), x=np.linspace(1, 15, 15), y=filt_pos_n)
-    .add(
-        so.Path(color="k", linewidth=2, linestyle="--"),
-        x=np.linspace(1, 15, 15),
-        y=len(filt_stats),
-        label="Total detectors",
-    )
-    .limit(x=(0.2, 15.8))
-    .label(
-        x="Number of simulations",
-        y="Number of detectors",
-        title="Detectors (5th percentile +) with significant results (97.5%)",
-    )
-    .save(OUTPUT_DIR / "plots" / "stat_signif_reduced.png")
+filt_num = [filt_stats[f"{n}"].sum() for n in range(1, max_sim_n + 1)]
+plot_det_prop(
+    filt_num,
+    len(filt_stats),
+    r"Meaningful detectors within 97.5% confidence level",
+    "stat_signif_filt.png",
 )
 
-
-prop_adj = []
-for n in t_n:
-    prop_adj.append((filt_stats[f"{n}"]).sum() / filt_stats.shape[0])
+# prop_adj = []
+# for n in range(1, max_sim_n + 1):
+#     prop_adj.append((filt_stats[f"{n}"]).sum() / filt_stats.shape[0])
