908057a3296c034dd90f9b44478e2918446fc649
Alessandro Scalese <ge48veb@mytum.de>
2026-01-26 21:44:03 +0100

    fix: update gof_eval in spsa to use new gof module

--- calibration/src/calibration/spsa.py
+++ calibration/src/calibration/spsa.py
@@ -10,7 +10,7 @@ from typing import Any, Self
 import numpy as np
 import pandas as pd
 
-from calibration import parsing, simulations, utils
+from calibration import gof, parsing, simulations, utils
 
 
 @dataclass(frozen=True)
@@ -101,7 +101,7 @@ def run_spsa(
     df_true = df_true.fillna(0)
 
     # evaluate goodness of fit
-    y = utils.gof_eval(df_true, df_simulated)
+    y = gof.gof_eval(df_true, df_simulated)
     print("Starting RMSN = ", y)
     print("========================================")
 
@@ -198,7 +198,7 @@ def run_spsa(
             plus_rel_maxs.append(float(np.nanmax(od_rel_plus)))
 
             df_simulated = run_parse_cleanup(config, sim_setup, OD_plus, seeds=seeds_ga)
-            y = utils.gof_eval(df_true, df_simulated)
+            y = gof.gof_eval(df_true, df_simulated)
             yplus = np.asarray(y)
             yplus_list.append(float(y))
 
@@ -239,7 +239,7 @@ def run_spsa(
             minus_rel_maxs.append(float(np.nanmax(od_rel_minus)))
 
             df_simulated = run_parse_cleanup(config, sim_setup, OD_minus, seeds=seeds_ga)
-            y = utils.gof_eval(df_true, df_simulated)
+            y = gof.gof_eval(df_true, df_simulated)
             yminus = np.asarray(y)
             yminus_list.append(float(y))
 
@@ -333,7 +333,7 @@ def run_spsa(
         # run simulation with updated OD
         print("Simulation %d . %d . minimization" % (iteration, ga))
         df_simulated = run_parse_cleanup(config, sim_setup, OD_min)
-        y_min = utils.gof_eval(df_true, df_simulated)
+        y_min = gof.gof_eval(df_true, df_simulated)
 
         rmsn.append(y_min)
 
